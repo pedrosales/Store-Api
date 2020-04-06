70d0dd511f64db07d71d3b63e0cd3b51825c3a49
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-06 01:00:58 -0300

    Criação do DataContext

---
+++ Store.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace Store.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=localhost;Database=Store;Trusted_Connection=True;";
+    }
+}
